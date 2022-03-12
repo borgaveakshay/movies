4b1e89323c7b8b8b7c53d6c802b93f04db9bf3ad
raveena <raveena.deshmane@nice.com>
2022-03-12 13:51:43 +0530

    code refactoring for test utils

---
+++ network_module/src/main/java/com/example/network_module/utils/TestUtils.kt
@@ -0,0 +1,9 @@
+package com.example.network_module.utils
+
+import com.example.network_module.data.datamodels.MoviesResponse
+
+object TestUtils {
+
+    fun getMovieResponse() = MoviesResponse(Actors = "Vinod","","","","","","")
+
+}
