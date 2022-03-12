8612fd3aa34d0071a37eb09a469e12ea0d452c59
raveena <raveena.deshmane@nice.com>
2022-03-12 11:58:19 +0530

    update the use case to adapt resource result object

--- network_module/src/main/java/com/example/network_module/domain/MoviesUseCase.kt
+++ network_module/src/main/java/com/example/network_module/domain/MoviesUseCase.kt
@@ -1,13 +1,12 @@
 package com.example.network_module.domain
 
 import com.example.network_module.data.datamodels.MoviesResponse
+import com.example.network_module.data.datamodels.ResultResource
 import com.example.network_module.data.repository.MoviesRepository
 
 class MoviesUseCase(private val repository: MoviesRepository) :
-    BaseUseCase<String, MoviesResponse>() {
-    override suspend fun createUseCaseExecution(req: String?): MoviesResponse {
-        return req?.let {
-            repository.getMovies(it)
-        } ?: MoviesResponse()
-    }
+    BaseUseCase<String, ResultResource<MoviesResponse?>>() {
+    override suspend fun createUseCaseExecution(req: String?) =
+        req?.let { repository.getMovies(req) }
+            ?: ResultResource.fail<MoviesResponse?>("request parameter cannot be null")
 }
