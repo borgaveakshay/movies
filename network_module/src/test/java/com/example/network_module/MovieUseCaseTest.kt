df30f0a11d03c993358ed6d5e3090dffd692adaf
raveena <raveena.deshmane@nice.com>
2022-03-12 11:55:24 +0530

    added error scenario test case for movie use case

--- network_module/src/test/java/com/example/network_module/MovieUseCaseTest.kt
+++ network_module/src/test/java/com/example/network_module/MovieUseCaseTest.kt
@@ -1,9 +1,10 @@
 package com.example.network_module
 
 import com.example.network_module.data.datamodels.MoviesResponse
-import com.example.network_module.data.datamodels.TestUtils
+import com.example.network_module.data.datamodels.ResultResource
 import com.example.network_module.data.repository.MoviesRepository
 import com.example.network_module.domain.MoviesUseCase
+import com.example.network_module.utils.TestUtils
 import com.google.common.truth.Truth
 import io.mockk.coEvery
 import io.mockk.mockkClass
@@ -24,19 +25,33 @@ class MovieUseCaseTest {
     }
 
     @Test
-    fun should_return_movies_for_use_case() {
+    fun `should return the result for movie use case`() {
         // GIVEN
-        coEvery { repository.getMovies("abc") } returns TestUtils.getMovieResponse()
+        coEvery { repository.getMovies("abc") } returns ResultResource.success(TestUtils.getMovieResponse())
 
         // WHEN
-        val result: MoviesResponse
+        val result: ResultResource<MoviesResponse?>
         runBlocking {
             result = useCase("abc")
         }
         // THEN
-        Truth.assertThat(result).isEqualTo(TestUtils.getMovieResponse())
+        Truth.assertThat(result.data).isEqualTo(TestUtils.getMovieResponse())
 
     }
 
+    @Test
+    fun `should return error for the movie use case`() {
+        // GIVEN
+        coEvery { repository.getMovies("abc") } returns ResultResource.fail<MoviesResponse?>("cannot fetch the records")
+
+        // WHEN
+        val result: ResultResource<MoviesResponse?>
+        runBlocking {
+            result = useCase("abc")
+        }
+        // THEN
+        Truth.assertThat(result.errorMessage).isEqualTo("cannot fetch the records")
+
+    }
 
 }
