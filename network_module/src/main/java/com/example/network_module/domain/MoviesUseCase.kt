1ef07bbbdd5cc309c425078a088b1641ce171ab9
raveena <raveena.deshmane@nice.com>
2022-03-12 14:11:08 +0530

    integrated hilt for the usecase

--- network_module/src/main/java/com/example/network_module/domain/MoviesUseCase.kt
+++ network_module/src/main/java/com/example/network_module/domain/MoviesUseCase.kt
@@ -3,8 +3,9 @@ package com.example.network_module.domain
 import com.example.network_module.data.datamodels.MoviesResponse
 import com.example.network_module.data.datamodels.ResultResource
 import com.example.network_module.data.repository.MoviesRepository
+import javax.inject.Inject
 
-class MoviesUseCase(private val repository: MoviesRepository) :
+class MoviesUseCase @Inject constructor(private val repository: MoviesRepository) :
     BaseUseCase<String, ResultResource<MoviesResponse?>>() {
     override suspend fun createUseCaseExecution(req: String?) =
         req?.let { repository.getMovies(req) }
