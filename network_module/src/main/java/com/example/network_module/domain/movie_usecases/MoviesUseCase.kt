83eaf77dd5379688824e347b3019d662e4697b27
raveena <raveena.deshmane@nice.com>
2022-03-12 14:16:29 +0530

    package movement

--- network_module/src/main/java/com/example/network_module/domain/movie_usecases/MoviesUseCase.kt
+++ network_module/src/main/java/com/example/network_module/domain/movie_usecases/MoviesUseCase.kt
@@ -1,8 +1,9 @@
-package com.example.network_module.domain
+package com.example.network_module.domain.movie_usecases
 
 import com.example.network_module.data.datamodels.MoviesResponse
 import com.example.network_module.data.datamodels.ResultResource
 import com.example.network_module.data.repository.MoviesRepository
+import com.example.network_module.domain.BaseUseCase
 import javax.inject.Inject
 
 class MoviesUseCase @Inject constructor(private val repository: MoviesRepository) :
