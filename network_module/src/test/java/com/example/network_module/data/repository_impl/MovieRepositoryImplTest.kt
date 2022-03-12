19bfdff8954c87d7e16ff4b40f3aa759a2e89835
raveena <raveena.deshmane@nice.com>
2022-03-12 13:50:52 +0530

    repository test cases

---
+++ network_module/src/test/java/com/example/network_module/data/repository_impl/MovieRepositoryImplTest.kt
@@ -0,0 +1,55 @@
+package com.example.network_module.data.repository_impl
+
+import com.example.network_module.data.MoviesAPI
+import com.example.network_module.data.datamodels.MoviesResponse
+import com.example.network_module.data.datamodels.ResultResource
+import com.example.network_module.data.repository.MoviesRepository
+import com.example.network_module.utils.TestUtils
+import com.google.common.truth.Truth
+import io.mockk.coEvery
+import io.mockk.mockkClass
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class MovieRepositoryImplTest {
+    private lateinit var repository: MoviesRepository
+    private lateinit var moviesAPI: MoviesAPI
+
+    @Before
+    fun setup() {
+        moviesAPI = mockkClass(MoviesAPI::class)
+        repository = MovieRepositoryImpl(moviesAPI)
+    }
+
+    @Test
+    fun `should return the result for movie repository`() {
+        // GIVEN
+        coEvery { moviesAPI.getMovieDetails("abc") } returns TestUtils.getMovieResponse()
+
+        // WHEN
+        val result: ResultResource<MoviesResponse?>
+        runBlocking {
+            result = repository.getMovies("abc")
+        }
+        // THEN
+        Truth.assertThat(result.data).isEqualTo(TestUtils.getMovieResponse())
+
+    }
+
+    @Test
+    fun `should return error for the movie repository`() {
+        // GIVEN
+        coEvery { moviesAPI.getMovieDetails("abc") } returns null
+
+        // WHEN
+        val result: ResultResource<MoviesResponse?>
+        runBlocking {
+            result = repository.getMovies("abc")
+        }
+        // THEN
+        Truth.assertThat(result.data).isEqualTo(null)
+
+    }
+
+}
