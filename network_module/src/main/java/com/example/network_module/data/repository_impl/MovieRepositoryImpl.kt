a84c114e2ef1e43474cb7531d308f5bbb5de7ffa
raveena <raveena.deshmane@nice.com>
2022-03-12 14:11:31 +0530

    integrated hilt for movie repository

--- network_module/src/main/java/com/example/network_module/data/repository_impl/MovieRepositoryImpl.kt
+++ network_module/src/main/java/com/example/network_module/data/repository_impl/MovieRepositoryImpl.kt
@@ -4,8 +4,9 @@ import com.example.network_module.data.MoviesAPI
 import com.example.network_module.data.datamodels.MoviesResponse
 import com.example.network_module.data.datamodels.ResultResource
 import com.example.network_module.data.repository.MoviesRepository
+import javax.inject.Inject
 
-class MovieRepositoryImpl(private val moviesAPI: MoviesAPI) : MoviesRepository {
+class MovieRepositoryImpl @Inject constructor(private val moviesAPI: MoviesAPI) : MoviesRepository {
     override suspend fun getMovies(search: String) = try {
         ResultResource.success(moviesAPI.getMovieDetails(search))
     } catch (e: Exception) {
